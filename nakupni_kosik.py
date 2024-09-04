f6cf013430e3becbc2d1c0d7e672ff749aff26d3
Petr Breit <104927244+PanUcitelCz@users.noreply.github.com>
2024-09-04 14:54:10 +0000

    Oprava

--- nakupni_kosik.py
+++ nakupni_kosik.py
@@ -1,5 +1,5 @@
 zbozi = ["CPU", "GPU", "RAM", "HDD", "SSD", "Motherboard", "Chlazení vodní", "Zdroj", "Case"]
-kosik =[]
+kosik = []
 
 def vypis_zbozi():
     print("---------------------------")
@@ -16,22 +16,42 @@ def vypis_kosik():
     print("---------------------------")
 
 def vypis_pole(prvek):
-    
     for x in range(len(prvek)): 
         print(f"Komponent s číslem {x+1}: {prvek[x]}")
-    
     print(" ")
     print("---------------------------")
 
+def pridat_do_kosiku_vyberem(vyber):
+    # Přidání do košíku podle čísla
+    kosik.append(zbozi[vyber-1])
+    zbozi.pop(vyber-1)
+
+def pridat_do_kosiku_nazvem(nazev):
+    # Přidání do košíku podle názvu, pokud se najde
+    if nazev in zbozi:
+        kosik.append(nazev)
+        zbozi.remove(nazev)
+    else:
+        print(f"Produkt s názvem '{nazev}' nebyl nalezen.")
 
 while(True):
+    if len(zbozi) == 0:
+        print("Už nemůžeš přidat další zboží, košík je plný!")
+        break
+
     vypis_zbozi()
     vypis_pole(zbozi)
 
-    vyber = int(input("Zde zadejte Vaší volbu produktu podle čísla: "))
+    volba = input("Zadejte číslo nebo název produktu, který chcete přidat do košíku: ")
 
-    if 0<vyber<=len(zbozi):
-        kosik.append(zbozi[vyber])
-        vypis_kosik()
-        vypis_pole(kosik)
-        zbozi.pop(vyber)
+    if volba.isdigit():  # Zda je volba číslo
+        vyber = int(volba)
+        if 0 < vyber <= len(zbozi):
+            pridat_do_kosiku_vyberem(vyber)
+        else:
+            print("Neplatná volba čísla.")
+    else:
+        pridat_do_kosiku_nazvem(volba)
+
+    vypis_kosik()
+    vypis_pole(kosik)
